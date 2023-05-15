d3ba18305076aadb9dc6a28d342019df382c2bf6
thanhdat <kimthanhdat0510@gmail.com>
2023-05-15 18:30:18 +0700

    refersh token

--- src/services/ApiServices.js
+++ src/services/ApiServices.js
@@ -1,122 +1,122 @@
-import getAxios from "~/util/axiosCustomize";
+import getAxios from "~/util/axiosCustomize"
 
 // Users
 export const CreateNewUser = (email, password, userName, role, avatar) => {
   // Call API submit data
-  const data = new FormData();
-  data.append("email", email);
-  data.append("password", password);
-  data.append("username", userName);
-  data.append("role", role);
-  data.append("avatar", avatar);
+  const data = new FormData()
+  data.append("email", email)
+  data.append("password", password)
+  data.append("username", userName)
+  data.append("role", role)
+  data.append("avatar", avatar)
 
-  return getAxios.post("v1/participant", data);
-};
+  return getAxios.post("/api/v1/participant", data)
+}
 
 export const getAllUser = () => {
-  return getAxios.get("v1/participant/all");
-};
+  return getAxios.get("/api/v1/participant/all")
+}
 
 export const getUserWithPaginate = (page, limit) => {
-  return getAxios.get(`v1/participant?page=${page}&limit=${limit}`);
-};
+  return getAxios.get(`/api/v1/participant?page=${page}&limit=${limit}`)
+}
 
 export const putUpdateUser = (id, userName, role, avatar) => {
-  const dataUpdate = new FormData();
-  dataUpdate.append("id", id);
-  dataUpdate.append("username", userName);
-  dataUpdate.append("role", role);
-  dataUpdate.append("userImage", avatar);
+  const dataUpdate = new FormData()
+  dataUpdate.append("id", id)
+  dataUpdate.append("username", userName)
+  dataUpdate.append("role", role)
+  dataUpdate.append("userImage", avatar)
 
-  return getAxios.put("v1/participant", dataUpdate);
-};
+  return getAxios.put("/api/v1/participant", dataUpdate)
+}
 
 export const deleteUser = (userId) => {
-  return getAxios.delete("v1/participant", { data: { id: userId } });
-};
+  return getAxios.delete("/api/v1/participant", { data: { id: userId } })
+}
 
 export const getQuizByUser = () => {
-  return getAxios.get("v1/quiz-by-participant");
-};
+  return getAxios.get("/api/v1/quiz-by-participant")
+}
 
 export const getDataOverView = () => {
-  return getAxios.get("v1/overview");
-};
+  return getAxios.get("/api/v1/overview")
+}
 
 export const getUserHistory = () => {
-  return getAxios.get("v1/history");
-};
+  return getAxios.get("/api/v1/history")
+}
 
 // Login + Signup
 export const postLogin = (email, password, delay) => {
-  return getAxios.post("v1/login", {
+  return getAxios.post("/api/v1/login", {
     email,
     password,
     // delay: 5000, // tạo delay cho khi đăng nhập
-  });
-};
+  })
+}
 
 export const postSignup = (email, password, username) => {
-  return getAxios.post("v1/register", {
+  return getAxios.post("/api/v1/register", {
     email,
     password,
     username,
-  });
-};
+  })
+}
 
 export const postLogout = (email, refresh_token) => {
-  return getAxios.post("v1/logout", {
+  return getAxios.post("/api/v1/logout", {
     email,
     refresh_token,
-  });
-};
+  })
+}
 
 export const postRefreshToken = (email, refresh_token) => {
-  return getAxios.post("v1/refresh-token", {
+  return getAxios.post("/api/v1/refresh-token", {
     email,
     refresh_token,
-  });
-};
+  })
+}
 
 export const postChangePassword = (current_password, new_password) => {
-  return getAxios.post("v1/change-password", {
+  return getAxios.post("/api/v1/change-password", {
     current_password,
     new_password,
-  });
-};
+  })
+}
 
 // Quiz test
 export const getDataQuiz = (id) => {
-  return getAxios.get(`v1/questions-by-quiz?quizId=${id}`);
-};
+  return getAxios.get(`/api/v1/questions-by-quiz?quizId=${id}`)
+}
 
 export const postSubmitQuiz = (data) => {
-  return getAxios.post("v1/quiz-submit", { ...data });
-};
+  return getAxios.post("/api/v1/quiz-submit", { ...data })
+}
 
 export const createNewQuiz = (description, name, difficulty, quizImage) => {
-  const data = new FormData();
-  data.append("description", description);
-  data.append("name", name);
-  data.append("difficulty", difficulty);
-  data.append("quizImage", quizImage);
+  const data = new FormData()
+  data.append("description", description)
+  data.append("name", name)
+  data.append("difficulty", difficulty)
+  data.append("quizImage", quizImage)
 
-  return getAxios.post("v1/quiz", data);
-};
+  return getAxios.post("/api/v1/quiz", data)
+}
 export const postUpdateProfile = (username, userImage) => {
-  const dataUpdate = new FormData();
-  dataUpdate.append("username", username);
-  dataUpdate.append("userImage", userImage);
-  return getAxios.post("v1/profile", dataUpdate);
-};
+  const dataUpdate = new FormData()
+  dataUpdate.append("username", username)
+  dataUpdate.append("userImage", userImage)
+  return getAxios.post("/api/v1/profile", dataUpdate)
+}
 
 export const getAllQuiz = () => {
-  return getAxios.get("v1/quiz/all");
-};
+  return getAxios.get("/api/v1/quiz/all")
+}
 
 export const deleteQuizForAdmin = (id) => {
-  return getAxios.delete(`v1/quiz/${id}`);
-};
+  return getAxios.delete(`/api/v1/quiz/${id}`)
+}
 
 export const putUpdateQuizForAdmin = (
   id,
@@ -125,49 +125,49 @@ export const putUpdateQuizForAdmin = (
   difficulty,
   quizImage
 ) => {
-  const dataUpdate = new FormData();
-  dataUpdate.append("id", id);
-  dataUpdate.append("description", description);
-  dataUpdate.append("name", name);
-  dataUpdate.append("difficulty", difficulty);
-  dataUpdate.append("quizImage", quizImage);
+  const dataUpdate = new FormData()
+  dataUpdate.append("id", id)
+  dataUpdate.append("description", description)
+  dataUpdate.append("name", name)
+  dataUpdate.append("difficulty", difficulty)
+  dataUpdate.append("quizImage", quizImage)
 
-  return getAxios.put("v1/quiz", dataUpdate);
-};
+  return getAxios.put("/api/v1/quiz", dataUpdate)
+}
 
 export const postAssignQuizForUser = (quizId, userId) => {
-  return getAxios.post("v1/quiz-assign-to-user", {
+  return getAxios.post("/api/v1/quiz-assign-to-user", {
     quizId,
     userId,
-  });
-};
+  })
+}
 
 export const getQuizWithQA = (quizId) => {
-  return getAxios.get(`v1/quiz-with-qa/${quizId}`);
-};
+  return getAxios.get(`/api/v1/quiz-with-qa/${quizId}`)
+}
 
 export const postUpsertQA = (data) => {
-  return getAxios.post("v1/quiz-upsert-qa", { ...data });
-};
+  return getAxios.post("/api/v1/quiz-upsert-qa", { ...data })
+}
 
 // Questions and answers
 
 export const postCreateNewQuestion = (quiz_id, description, questionImage) => {
-  const data = new FormData();
-  data.append("quiz_id", quiz_id);
-  data.append("description", description);
-  data.append("questionImage", questionImage);
-  return getAxios.post("v1/question", data);
-};
+  const data = new FormData()
+  data.append("quiz_id", quiz_id)
+  data.append("description", description)
+  data.append("questionImage", questionImage)
+  return getAxios.post("/api/v1/question", data)
+}
 
 export const postCreateNewAnswer = (
   description,
   correct_answer,
   question_id
 ) => {
-  return getAxios.post("v1/answer", {
+  return getAxios.post("/api/v1/answer", {
     description,
     correct_answer,
     question_id,
-  });
-};
+  })
+}
--- src/util/axiosCustomize.js
+++ src/util/axiosCustomize.js
@@ -1,23 +1,38 @@
-import axios from "axios";
-import { store } from "~/redux/store";
+import axios from "axios"
+import { store } from "~/redux/store"
+
+const baseURL = process.env.BACKEND_URL
+
+const NO_RETRY_HEADER = "x-no-retry"
 
 const getAxios = axios.create({
-  baseURL: "http://localhost:8081/api/",
-});
+  baseURL: baseURL,
+  withCredentials: true,
+})
+
+// call api to refresh token
+const handleRefreshToken = async () => {
+  try {
+    const res = await getAxios.get("/api/v1/refresh-token")
+    if (res && res.DT) return res.DT.access_token
+  } catch (e) {
+    console.log("Error", e)
+  }
+}
 
 getAxios.interceptors.request.use(
   function (config) {
-    const access_token = store?.getState()?.user?.account?.access_token;
-    config.headers["Authorization"] = `Bearer ${access_token}`;
+    const access_token = store?.getState()?.user?.account?.access_token
+    config.headers["Authorization"] = `Bearer ${access_token}`
 
     // Do something before request is sent
-    return config;
+    return config
   },
   function (error) {
     // Do something with request error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 // Add a response interceptor
 getAxios.interceptors.response.use(
@@ -25,15 +40,42 @@ getAxios.interceptors.response.use(
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
 
-    return response && response.data ? response.data : response;
+    return response && response.data ? response.data : response
   },
-  function (error) {
+  async function (error) {
+    // refresh token
+    if (
+      error?.config &&
+      error.response &&
+      +error.response.status === 401 &&
+      !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      const access_token = await handleRefreshToken()
+      error.config.headers[NO_RETRY_HEADER] = "true"
+      if (access_token) {
+        console.log("refresh token")
+        localStorage.setItem("access_token", access_token)
+        error.config.headers["Authorization"] = `Bearer ${access_token}`
+        return getAxios.request(error.config)
+      }
+    }
+
+    // khi hết thời gian của refresh token
+
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 400 &&
+      error.config.url === "/api/v1/auth/refresh"
+    ) {
+      window.location.href = "/login"
+    }
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response && error.response.data
       ? error.response.data
-      : Promise.reject(error);
+      : Promise.reject(error)
   }
-);
+)
 
-export default getAxios;
+export default getAxios
